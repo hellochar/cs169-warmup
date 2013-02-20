38ef6f27e6e72acf75b8938d9a35457aa6b07c02
Xiaohan Zhang <hellocharlien@hotmail.com>
2013-02-19 16:48:20 -0800

    use better user/password combo

--- warmup/settings.py
+++ warmup/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'cs169_warmup_db',                      # Or path to database file if using sqlite3.
-        'USER': 'hellochar',                      # Not used with sqlite3.
-        'PASSWORD': 'madcows',                  # Not used with sqlite3.
+        'USER': 'cs169_warmup_user',                      # Not used with sqlite3.
+        'PASSWORD': 'password1',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
